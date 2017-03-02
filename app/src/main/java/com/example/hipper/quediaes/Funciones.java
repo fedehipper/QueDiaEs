e462162d2a140e06ecce57b002d093499a697a46
fedehipper <fedehipper@gmail.com>
2017-03-02 02:13:57 -0300

    correccion fechas

--- app/src/main/java/com/example/hipper/quediaes/Funciones.java
+++ app/src/main/java/com/example/hipper/quediaes/Funciones.java
@@ -4,104 +4,48 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.javatuples.Pair;
-import org.javatuples.Triplet;
+import org.joda.time.DateTime;
 
 public class Funciones {
-    private List<Triplet<String,Integer,String>> meses = new ArrayList<>();
     private List<Pair<String, Integer>> dias = new ArrayList<>();
+    private List<Pair<String,Integer>> meses = new ArrayList<>();
 
     public Funciones() {
-        meses = Arrays.asList(new Triplet<>("ENERO", 6, "1"), new Triplet<>("FEBRERO", 2, "2"),
-                new Triplet<>("MARZO", 2, "3"), new Triplet<>("ABRIL", 5, "4"), new Triplet<>("MAYO", 0, "5"),
-                new Triplet<>("JUNIO", 3, "6"), new Triplet<>("JULIO", 5, "7"), new Triplet<>("AGOSTO", 1, "8"),
-                new Triplet<>("SEPTIEMBRE", 4, "8"), new Triplet<>("OCTUBRE", 6, "9"), new Triplet<>("NOVIEMBRE", 4, "10"),
-                new Triplet<>("DICIEMBRE", 4, "12"));
+        meses = Arrays.asList(new Pair<>("ENERO", 1), new Pair<>("FEBRERO", 2),
+                new Pair<>("MARZO", 3), new Pair<>("ABRIL", 4), new Pair<>("MAYO", 5),
+                new Pair<>("JUNIO", 6), new Pair<>("JULIO", 7), new Pair<>("AGOSTO", 8),
+                new Pair<>("SEPTIEMBRE", 10), new Pair<>("OCTUBRE", 9),
+                new Pair<>("NOVIEMBRE", 11), new Pair<>("DICIEMBRE", 12));
 
         dias = Arrays.asList(new Pair<>("Domingo", 0), new Pair<>("Lunes", 1), new Pair<>("Martes", 2),
-                new Pair<>("Miercoles", 3), new Pair<>("Jueves", 4), new Pair<>("Viernes", 5), new Pair<>("Sabado", 6));
-    }
-
-    private List<Integer> llenarDeAnios(int rangoInicial, int rangoFinal, List<Integer> lista) {
-        for(int i = rangoInicial ; i <= rangoFinal ; i++) lista.add(i); return lista;
-    }
-
-    private Pair<List<Integer>, Integer> tuplaSiglo(Integer unAnio) {
-        Integer siglo = (unAnio / 100) + 1;
-        Integer valorSiglo;
-        if(siglo >= 21) {
-            valorSiglo = (siglo - 21) * -2;
-        } else {
-            List<Integer> listaImpares = new ArrayList<>();
-            for(Integer i = 0 ; i < 50 ; i++) {
-                if(i % 2 == 0) {
-                    listaImpares.add(i);
-                }
-            }
-            valorSiglo = listaImpares.get(21 - siglo);
-        }
-        List<Integer> rangoSiglo = new ArrayList<>();
-        llenarDeAnios((siglo - 1) * 100, ((siglo - 1) * 100) + 99, rangoSiglo);
-        return new Pair<>(rangoSiglo, valorSiglo);
-    }
-
-    private Integer coeficienteA(Integer unAnio) {
-        return (Integer)tuplaSiglo(unAnio).getValue(1);
-    }
-
-    private Integer dosUltimosDigitos(Integer unAnio) {
-        return unAnio % 100;
-    }
-
-    private Integer unCuarto(Integer unValor) {
-        return unValor / 4;
-    }
-
-    private Integer coeficienteB(Integer unAnio) {
-        return dosUltimosDigitos(unAnio) + unCuarto(dosUltimosDigitos(unAnio));
-    }
-
-    private boolean multiplo(Integer unValor, Integer otroValor) {
-        return unValor % otroValor == 0 || otroValor % unValor == 0;
-    }
-
-    private boolean anioBisiesto(Integer unAnio) {
-        return (dosUltimosDigitos(unAnio) != 0) ? multiplo(dosUltimosDigitos(unAnio), 4): multiplo(dosUltimosDigitos(unAnio), 400);
-    }
-
-    private Integer coeficienteC(Integer unAnio, String unMes) {
-        return (anioBisiesto(unAnio) && Arrays.asList("Enero", "Febrero", "1", "2").contains(unMes)) ? - 1 : 0;
-    }
-
-    private Integer obtenerValorString(Triplet<String,Integer,String> triplet) {
-        return (Integer)triplet.getValue(1);
-    }
-
-    private Integer coeficienteD(String unMes) {
-        List<Triplet<String,Integer,String>> mesesAux = new ArrayList<>();
-        for(int i = 0 ; i < this.meses.size() ; i++) {
-            if(Arrays.asList(this.meses.get(i).getValue(0), this.meses.get(i).getValue(2)).contains(unMes)) {
-                mesesAux.add(this.meses.get(i));
-            }
-        }
-        return obtenerValorString(mesesAux.get(0));
-    }
-
-    private Integer aplicarAlgoritmo(Integer unAnio, String unMes, Integer unDia) {
-        return (coeficienteA(unAnio) + coeficienteB(unAnio) + coeficienteC(unAnio, unMes.toUpperCase()) +
-                coeficienteD(unMes.toUpperCase()) + unDia) % 7;
+                new Pair<>("Miercoles", 3), new Pair<>("Jueves", 4), new Pair<>("Viernes", 5),
+                new Pair<>("Sabado", 6));
     }
 
     private String obtenerCadena(Pair<String, Integer> tupla) {
         return (String)tupla.getValue(0);
     }
 
-    public String calculoDeDia(Integer unAnio, String unMes, Integer unDia) {
+    public String calculoDeDia(Integer anio, String mes, Integer dia) {
+
+        Integer conversionDeMes = 0;
+        if(Arrays.asList("1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12").contains(mes)) {
+            conversionDeMes = Integer.valueOf(mes);
+        } else {
+            for(int i = 0 ; i < 12 ; i++) {
+                if(meses.get(i).getValue(0).equals(mes.toUpperCase())) {
+                    conversionDeMes = (Integer)meses.get(i).getValue(1);
+                }
+            }
+        }
+
+        DateTime date = new DateTime(anio, conversionDeMes, dia, 0, 0, 0);
+        Integer diaSemana = date.dayOfWeek().get();
 
         List<Pair<String, Integer>> diasAux = new ArrayList<>();
-
-        for(int i = 0 ; i < this.dias.size() ; i++) {
-            if(this.dias.get(i).getValue(1) == aplicarAlgoritmo(unAnio, unMes, unDia)) {
-                diasAux.add(this.dias.get(i));
+        for(int i = 0 ; i < 7 ; i++) {
+            if(dias.get(i).getValue(1) == diaSemana) {
+                diasAux.add(dias.get(i));
             }
         }
         return obtenerCadena(diasAux.get(0));
